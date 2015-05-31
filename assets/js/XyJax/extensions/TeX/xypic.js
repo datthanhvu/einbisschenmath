68f6755106ce057cd87f0a498c87f54d2860bfb8
wangjiezhe <wangjiezhe@gmail.com>
2015-05-31 22:33:36 +0800

    Bugfix for XyJax

--- assets/js/XyJax/extensions/TeX/xypic.js
+++ assets/js/XyJax/extensions/TeX/xypic.js
@@ -16769,4 +16769,4 @@ MathJax.Hub.Register.StartupHook("SVG Xy-pic Require",function () {
   MathJax.Hub.Startup.signal.Post("SVG Xy-pic Ready");
 });
 
-MathJax.Ajax.loadComplete("http://einbisschenmath.me/assets/js/XyJax/extensions/TeX/xypic.js");
+MathJax.Ajax.loadComplete("//einbisschenmath.me/assets/js/XyJax/extensions/TeX/xypic.js");
